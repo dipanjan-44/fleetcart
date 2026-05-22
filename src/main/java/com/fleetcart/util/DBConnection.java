f21d469cdb332e108941ee2c508321501cfa3b1f
Dipanjan Chakraborty <dipanjanchakab2255@gmail.com>
2026-05-22 23:59:10 +0530

    Fix production database connection

--- src/main/java/com/fleetcart/util/DBConnection.java
+++ src/main/java/com/fleetcart/util/DBConnection.java
@@ -19,14 +19,20 @@ public class DBConnection {
                 String username = System.getenv("DB_USERNAME");
                 String password = System.getenv("DB_PASSWORD");
 
+                System.out.println("DB URL: " + url);
+
                 connection = DriverManager.getConnection(
                         url,
                         username,
                         password
                 );
+
+                System.out.println("DATABASE CONNECTED SUCCESSFULLY");
             }
 
         } catch (Exception e) {
+
+            System.out.println("DB CONNECTION FAILED");
             e.printStackTrace();
         }
 
